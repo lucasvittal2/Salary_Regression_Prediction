e50fc9196441bda574ce27c74b02b950036ed5dc
lucasvittal2 <lucasvittal@gmail.com>
2023-06-12 14:46:03 -0300

    Fiinishing Project

--- Env/EnvVars.py
+++ Env/EnvVars.py
@@ -2,7 +2,7 @@
 #PATHS
 MODELS_PATH='../Models/'
 DATASET_PATH= "../Dataset/"
-
+PROJECT_PATH="/content/drive/MyDrive/Estudos/Pos-graduacao - Ciencia de Dados e  Analytics/Sprints/Sprint II - Machine Learning and Advanced Analytics/Previsao Salarios Com  Regresaao/Code"
 #Paramters
 
 TEST_SIZE= 0.2
